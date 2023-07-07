2393c252cb4a79b57b750701ca24ee56eda39085
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-07-07 22:43:39 +0600

    queryObject testing by params

--- src/api/controllers/productController.js
+++ src/api/controllers/productController.js
@@ -1,10 +1,20 @@
+const ProductModel = require("../models/ProductModel");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("Testing async Errors!");
-  res.status(200).json({ msg: "Products Testing Route" });
+  // throw new Error("Testing async Errors!");
+  const findingProducts = await ProductModel.find({ page: "2" });
+  res.status(200).json({ findingProducts, nbHits: findingProducts.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products Route" });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  const queryProducts = await ProductModel.find(queryObject);
+  res.status(200).json({ queryProducts, nbHits: queryProducts.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
